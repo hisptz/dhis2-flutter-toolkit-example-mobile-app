d2e6e6f70e4383fb22cd9585911fca41b847c038
Allen7702 <allen22052000@gmail.com>
2024-06-19 10:45:55 +0300

    feat(app.dart): added notifiers

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,7 @@
+import 'package:dhis2_flutter_toolkit_demo_app/app_state/app_module_data/app_module_data.dart';
 import 'package:dhis2_flutter_toolkit_demo_app/app_state/client_provider/client_provider.dart';
 import 'package:dhis2_flutter_toolkit_demo_app/app_state/db_provider/db_provider.dart';
+import 'package:dhis2_flutter_toolkit_demo_app/app_state/module_selection/module_selection.dart';
 import 'package:dhis2_flutter_toolkit_demo_app/app_state/user_state/user_state.dart';
 import 'package:dhis2_flutter_toolkit_demo_app/route.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +15,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+         ChangeNotifierProvider(
+          create: (_) => AppModuleSelectionState(),
+        ),
+        ChangeNotifierProxyProvider<AppModuleSelectionState,
+                SelectedAppModuleDataState>(
+            create: (_) => SelectedAppModuleDataState(),
+            update: (_, appModule, appModuleData) {
+              return SelectedAppModuleDataState.onAppModuleChange(
+                  appModule.selectedAppModule);
+            }),
         ChangeNotifierProvider(create: (_) => AuthState()),
         ChangeNotifierProvider<DBState>(
           create: (_) => DBState(),
