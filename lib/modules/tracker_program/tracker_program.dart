7c3960ddcc103bee707929d0cc4d753b4a680dd8
Allen7702 <allen22052000@gmail.com>
2024-06-19 11:16:11 +0300

    feat(tracker_program.dart): added a tracker program page

---
+++ lib/modules/tracker_program/tracker_program.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class TrackerProgramHome extends StatelessWidget {
+  const TrackerProgramHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
