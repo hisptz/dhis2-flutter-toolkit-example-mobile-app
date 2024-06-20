ef7d79d6c961174fa621de81303ffc9b3adc2753
Allen7702 <allen22052000@gmail.com>
2024-06-20 15:18:52 +0300

    feat(constants): added app constants

---
+++ lib/core/constants/app_info_reference.dart
@@ -0,0 +1,5 @@
+class AppInfoReference {
+  static const String androidId = 'org.hisptanzania';
+  static const String currentAppName = 'Flutter Toolkit Demo App';
+  static const String currentAppVersion = '1.0.0-beta1';
+}
---
+++ lib/core/constants/custom_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const Color appBackgroundColor = Color(0xFFE9ECEF);
+  static const Color appColor = Color(0xFF023047);
+  static const Color preferenceIndicatorColor = Color(0xFFC81314);
+  static const Color primaryColor = Color(0xFF2C6693);
+}
