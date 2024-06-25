45830f76e0dde0fe76de4f73c009450f15dc05ff
Allen7702 <allen22052000@gmail.com>
2024-06-25 10:04:02 +0300

    feat(app_module_helper.dart): added helper for list card

---
+++ lib/core/utils/module_helpers/app_module_helper.dart
@@ -0,0 +1,43 @@
+import 'package:dhis2_flutter_toolkit_demo_app/core/components/list_card_button.dart';
+import 'package:dhis2_flutter_toolkit_demo_app/models/app_module.dart';
+import 'package:flutter/material.dart';
+
+abstract class ListData {}
+
+class ListCardData extends ListData {
+  String id;
+  String title;
+  String? svgIcon;
+  String? date;
+  bool? syncStatus;
+  OnPressed onSync;
+  Map<String, dynamic>? referralFields;
+  Map<String, String>? fields;
+  List<Widget>? actionButtons;
+  MainAxisAlignment? actionButtonAlignment;
+
+  ListCardData({
+    required this.id,
+    required this.title,
+    required this.onSync,
+    this.svgIcon,
+    this.date,
+    this.syncStatus,
+    this.fields,
+    this.referralFields,
+    this.actionButtons,
+    this.actionButtonAlignment,
+  });
+}
+
+abstract class BaseAppModuleHelper<T> {
+  AppModule? selectedAppModule;
+
+  abstract List<Map<String, String>> fieldConfig;
+
+  ListCardData toListCard(T entity);
+
+  void setModule(AppModule module) {
+    selectedAppModule = module;
+  }
+}
